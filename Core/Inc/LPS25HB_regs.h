91182a1af8a8943e5072eba0820a4b4312a1df70
Ozkerror <ozkerror@gmail.com>
2026-02-23 19:47:50 +0100

    dodanie podstawowych adresow czujnika

---
+++ Core/Inc/LPS25HB_regs.h
@@ -0,0 +1,27 @@
+/*
+ * LPS25HB_regs.h
+ *
+ *  Created on: Feb 23, 2026
+ *      Author: ozker
+ */
+
+#ifndef INC_LPS25HB_REGS_H_
+#define INC_LPS25HB_REGS_H_
+
+
+#define LPS25HB_ADDR 0xBA
+#define LPS25HB_WHO_AM_I 0x0F
+#define LPS25HB_CTRL_REG1 0x20
+#define LPS25HB_CTRL_REG2 0x21
+#define LPS25HB_CTRL_REG3 0x22
+#define LPS25HB_CTRL_REG4 0x23
+#define LPS25HB_PRESS_OUT_XL 0x28
+#define LPS25HB_PRESS_OUT_L 0x29
+#define LPS25HB_PRESS_OUT_H 0x2A
+#define LPS25HB_TEMP_OUT_L 0x2B
+#define LPS25HB_TEMP_OUT_H 0x2C
+
+
+
+
+#endif /* INC_LPS25HB_REGS_H_ */
